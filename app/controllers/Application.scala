7ff7798a0033843d5153bf956465ea367c31319c
Nicolae Namolovan <nna@zenexity.com>
2012-08-31 15:59:54 +0200

    Streaming of the longitude,latitude. Heavily inspired from play2-wiki-growth-sse

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,16 @@
 package controllers
 
 import play.api._
+
 import play.api.mvc._
+import play.api.libs.ws.WS
+import play.api.Play.current
+import libs.iteratee.Enumeratee
+import libs.json.JsValue
+import libs.json.Json._
+import libs.EventSource
+import models.Coordinate
+import models.CoordinatesStream._
 
 object Application extends Controller {
 
@@ -9,11 +18,17 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
+  val asJson: Enumeratee[Coordinate, JsValue] = Enumeratee.map[Coordinate] {
+    coordinate => toJson ( Map (
+      "latitude" -> toJson(coordinate.latitude.toString()),
+      "longitude" -> toJson(coordinate.longitude.toString())
+    ) )
+  }
+
   /**
     * Stream of server send events
    */
   def stream() = Action {
-    Ok.stream(/* Nicolae JSON as Enumaratee or Iteratee I dont remember */ ><> EventSource()).as("text/event-stream")
+    Ok.stream(fileLineStream(Play.getExistingFile("conf/play.json").get) &> lineParser &> validCoordinate &> asJson ><> EventSource()).as("text/event-stream")
   }
-
 }
---
+++ app/models/CoordinatesStream.scala
@@ -0,0 +1,46 @@
+package models
+
+import java.io.File
+import play.api._
+import play.api.libs.iteratee.{Input, Enumeratee, Enumerator}
+import play.api.libs.concurrent.Promise
+import play.api.Logger
+import play.api.libs.json
+import play.api.libs.json.Json
+import play.api.libs.json.JsArray
+
+case class Coordinate(latitude: BigDecimal, longitude: BigDecimal)
+
+object CoordinatesStream {
+
+  def fileLineStream(file: File) : Enumerator[String] = {
+    val source = scala.io.Source.fromFile(file)
+    val lines = source.getLines()
+
+    Enumerator.fromCallback[String] (() => {
+      val line = if (lines.hasNext) {
+        Some(lines.next())
+      } else {
+        None
+      }
+      Promise.pure(line)
+    }, source.close)
+  }
+
+  val lineParser: Enumeratee[String, Option[Coordinate]] = Enumeratee.map[String] {
+    line =>
+      val elements = Json.parse(line)
+      try {
+        Some(Coordinate(latitude  = (elements \ "latitude").as[String].toDouble,
+                        longitude = (elements \ "longitude").as[String].toDouble))
+      } catch {
+        case e: Exception => Logger.error("error while parsing line: " + line, e); None
+      }
+  }
+
+  val validCoordinate: Enumeratee[Option[Coordinate], Coordinate] = Enumeratee.mapInput[Option[Coordinate]] {
+    case Input.El(maybe) if maybe.isDefined => Input.El(maybe.get)
+    case other => Input.Empty // ignore invalid coordinates
+  }
+
+}
