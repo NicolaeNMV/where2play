e049a03008dd02d45968a0cb183d81c438f61033
Mariot Chauvin <mariot.chauvin@gmail.com>
2012-08-31 12:25:33 +0200

    boostrap ui

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,16 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
-  
+
+  /**
+    * Stream of server send events
+   */
+  def stream() = Action {
+    Ok.stream(/* Nicolae JSON as Enumaratee or Iteratee I dont remember */ ><> EventSource()).as("text/event-stream")
+  }
+
 }
